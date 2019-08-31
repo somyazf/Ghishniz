0783757083042955239d4a5bc467053462799ac6
sazgit <s.azfard@gmail.com>
2019-08-31 16:51:44 +0430

    add redux folders

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './_components/header';
+import './App.css';
+
+const App = () =>{
+    return<>
+        <Header/>
+      </>
+    
+}
+export default App;
