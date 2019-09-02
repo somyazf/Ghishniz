b39e445bb0da4b4c6e4152370c680fe1d788e110
sazgit <s.azfard@gmail.com>
2019-09-03 00:20:52 +0430

    add title

---
+++ src/_components/mainPage/title/title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from '../../../App.css';
+
+const Title =(props)=> {
+    return(
+        <p className="pSuggestStyle">{props.title}</p>
+    )
+}
+
+
+export default Title;
