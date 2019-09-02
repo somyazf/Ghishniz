8e3924771e02685b018459915d44326ea0ba0bd6
sazgit <s.azfard@gmail.com>
2019-09-03 00:22:10 +0430

    added Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './_components/mainPage/home';
 import Header from './_components/header';
+import Footer from './_components/footer';
 import './App.css';
 
-const App = () =>{
+const App = (props) =>{
     return<>
-        <Header/>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/header" component={Header}/>
+            <Route path="/footer" component={Footer}/>
+          </Switch>
+        </Router>
       </>
     
 }
