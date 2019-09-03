1ebffd8be14e4ff2e9b44296f1b48bebea8fab14
sazgit <s.azfard@gmail.com>
2019-09-03 22:57:42 +0430

    added rating icon

---
+++ src/_components/mainPage/icons/rating.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Rating from "@material-ui/lab/Rating";
+
+export default function SimpleRating() {
+  const [value, setValue] = React.useState(2);
+  return (
+    <div>
+      <Rating
+        name="simple-controlled"
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      />
+    </div>
+  );
+}
