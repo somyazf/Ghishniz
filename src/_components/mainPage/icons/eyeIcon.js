a64429814fe460fd6954380f1a250f2666a12b14
sazgit <s.azfard@gmail.com>
2019-09-03 22:57:19 +0430

    added eye icon

---
+++ src/_components/mainPage/icons/eyeIcon.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import IconButton from "@material-ui/core/IconButton";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({ 
+    root: {
+      color: theme.palette.text.primary,
+    },
+    icon: {
+      
+      fontSize: 24,
+    },
+}));
+export default function EyeIcon() {
+    const classes = useStyles();
+    return (
+        <IconButton>
+            <VisibilityIcon className={classes.icon} />
+        </IconButton>
+    )
+}
