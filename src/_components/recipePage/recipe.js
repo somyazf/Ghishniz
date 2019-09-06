d0c48a3db92b4a80010ca8915e2adead8063f409
sazgit <s.azfard@gmail.com>
2019-09-06 19:53:50 +0430

    single recipe

---
+++ src/_components/recipePage/recipe.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { Link } from "react-router-dom";
+
+const SpoonacularApi = "94be430aadf644f6a8c8c95abbcce4c1";
+
+class Recipe extends React.Component {
+  state = {
+    activeRecipe: []
+  }
+  componentDidMount = async () => {
+    const title = this.props.location.state.recipe;
+    const req = await fetch(`https://cors-anywhere.herokuapp.com/{baseUrl}/recipes/search?apiKey=${SpoonacularApi}&query=${title}`);
+    
+    const res = await req.json();
+    this.setState({ activeRecipe: res.recipes[0] });
+    console.log(this.state.activeRecipe);
+  }
+  render() {
+    const recipe = this.state.activeRecipe;
+    return (
+      <div className="container">
+        { this.state.activeRecipe.length !== 0 &&
+          <div className="active-recipe">
+            <img className="active-recipe__img" src={recipe.imageUrls[0]} alt={recipe.title}/>
+            <h3 className="active-recipe__title">{ recipe.title }</h3>
+            <h4 className="active-recipe__publisher">
+            readyInMinutes: <span>{ recipe.readyInMinutes }</span>
+            </h4>
+            <p className="active-recipe__website">servings: 
+              <span>{recipe.servings}</span>
+            </p>
+            <button className="active-recipe__button">
+              <Link to="/">Go Home</Link>
+            </button>
+          </div>
+        }
+      </div>
+    );
+  }
+};
+
+export default Recipe;
