5fbb2ba48a13c3ea18551451b3cdd3c8ac94df2a
sazgit <s.azfard@gmail.com>
2019-09-09 22:11:03 +0430

    single recipe design

--- src/_components/recipePage/recipe.js
+++ src/_components/recipePage/recipe.js
@@ -1,44 +1,143 @@
 import React from 'react';
-
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+import Rate from "../mainPage/icons/rating";
+import EyeIcon from "../mainPage/icons/eyeIcon";
+import FavoriteFood from "../mainPage/icons/favorite";
+import Divider from '@material-ui/core/Divider';
+import sug1 from '../../assets/sug1.jpg';
 import { Link } from "react-router-dom";
 
 const SpoonacularApi = "94be430aadf644f6a8c8c95abbcce4c1";
 const baseUrl = "https://api.spoonacular.com";
 const imgUrl = "https://webknox.com/recipeImages/";
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    padding: 60,
+  },
+  flexDivs: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  divider: {
+    marginBottom: '33px',
+    height: '3px',
+    width: '13%',
+    margin: '10px auto',
+    backgroundColor: '#0ec145'
+  },
+  recipeImg: {
+    maxWidth: 480,
+    maxHeight: 480,
+    width: '35vw',
+    height: '35vw',
+    backgroundSize: 'cover',
+    borderRadius: 10,
+  },
+  instButton: {
+    backgroundColor:' #78b507',
+  }
+}));
 
-class Recipe extends React.Component {
-  state = {
-    activeRecipe: []
-  }
-  componentDidMount = async () => {
-    const title = this.props.location.state.recipe;
-    const req = await fetch(`${baseUrl}/recipes/search?apiKey=${SpoonacularApi}&query=${title}`);
-    const res = await req.json();
-    this.setState({ activeRecipe: res.results[0] });
-    console.log(this.state.activeRecipe);
-  }
-  render() {
-    const recipe = this.state.activeRecipe;
-    return (
-      <div className="container">
-        { this.state.activeRecipe.length !== 0 &&
-          <div className="active-recipe">
-            <img className="active-recipe__img" src={`${imgUrl}${recipe.image}`} alt={recipe.title}/>
-            <h3 className="active-recipe__title">{ recipe.title }</h3>
-            <h4 className="active-recipe__publisher">
-            readyInMinutes: <span>{ recipe.readyInMinutes }</span>
-            </h4>
-            <p className="active-recipe__website">servings: 
-              <span>{recipe.servings}</span>
-            </p>
-            <button className="active-recipe__button">
-              <Link to="/recipes">Go Home</Link>
-            </button>
+const Recipe = () =>  {
+  const classes = useStyles();
+    return <>
+      <Grid container spacing={2}>
+        <Grid item xs>
+          sidebar
+        </Grid>
+        <Grid item xs={10} >
+          <Grid className={classes.root}>
+            <Grid item lg={6}>
+              <Typography variant="h4" component="h3" align="left" gutterBottom>
+                Recipe Title
+              </Typography>
+              <Rate/>
+              <Grid className={classes.flexDivs}>
+                <Grid className="ingredients">
+                  <Typography variant="subtitle1" align="center" gutterBottom>
+                    7
+                  </Typography>
+                  <Typography variant="subtitle1" align="center" gutterBottom>
+                    Ingredients
+                  </Typography>
+                </Grid>
+                <Divider orientation="vertical"/>
+                <Grid className="time">
+                  <Typography variant="subtitle1" align="center" gutterBottom>
+                    40
+                  </Typography>
+                  <Typography variant="subtitle1" align="center" gutterBottom>
+                    Minutes
+                  </Typography>
+                </Grid>
+                <Divider orientation="vertical"/>
+                <Grid className="calories">
+                  <Typography variant="subtitle1" align="center" gutterBottom>
+                      ┅
+                  </Typography>
+                  <Typography variant="subtitle1" align="center" gutterBottom>
+                    Calories
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid className={classes.flexDivs}>
+                <div>
+                <Fab variant="extended" href="#direction" className={classes.instButton}>
+                  read instruction
+                </Fab>
+                </div>
+                <EyeIcon/>
+              </Grid>
+            </Grid>
+            <Grid item lg={6}>
+              <img src={sug1} alt="recipe img" className={classes.recipeImg}/>
+            </Grid>
+          </Grid>
+          <div>
+          <Divider/>
           </div>
-        }
-      </div>
-    );
-  }
+          <Grid container lg = "6">
+              <Grid>
+                <Typography variant="h3" gutterBottom>
+                  Ingredients
+                </Typography>
+                <Typography variant="subtitle" gutterBottom>
+                  - 4 bone-in ribeye pork chops (rib, 3/4-inch thick)
+                  - 2 cloves garlic (peeled)
+                  - 1 cup fresh basil (packed)
+                  - 2 tablespoons lemon juice (fresh)
+                  - 2 tablespoons extra virgin olive oil
+                 -  1 teaspoon coarse salt
+                  - 1/2 teaspoon freshly ground black pepper
+                </Typography>
+              </Grid>
+              <Grid>
+                <Typography variant="h3" gutterBottom id= "direction">
+                  Directions
+                </Typography>
+                <Typography variant="subtitle" gutterBottom>
+                  With machine running, drop garlic through feed tube of food processor to mince. Stop, add fresh basil, and process until chopped. Add lemon juice, oil, salt, and pepper and process to make thin wet rub. Spread both sides of pork chops with basil mixture. Let stand 15 to 30 minutes.
+                  Prepare a medium-hot fire in grill. Brush the grate clean and oil the grate. Grill chops, over direct heat, turning once, to medium rare doneness, 5 to 6 minutes per side, or until the internal temperature reaches 145 degrees Fahrenheit, followed by a 3-minute rest time.
+                </Typography>
+            </Grid>
+            <Grid>
+                <Typography variant="h3" gutterBottom>
+                  Reviews(126)
+                </Typography>
+                <Typography variant="subtitle" gutterBottom>
+                Tim T. 5 days ago
+Definitely top with toasted pine nuts and shaved/grated Parmesan! Makes all the difference!!!
+                </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
 };
 
 export default Recipe;
