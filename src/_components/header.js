95733229694c89fcf38d57928b56da463b413d92
sazgit <s.azfard@gmail.com>
2019-09-11 21:07:15 +0430

    add webRouter

--- src/_components/header.js
+++ src/_components/header.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import useWindowScrollPosition from "@rehooks/window-scroll-position";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, withRouter } from "react-router-dom";
 import '../App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Icon from '@material-ui/core/Icon';
@@ -100,4 +100,5 @@ function TestNav() {
       <Header/>
     );
   }
-export default Header;
+withRouter(Header);
+export {Header};
