3f6239ae3d2273fd68fe451a2ecf9517b6075dd0
sazgit <s.azfard@gmail.com>
2019-09-12 23:10:40 +0430

    create navBar component

---
+++ src/_components/header/navBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../App.css";
+
+function NavBarMenu() {
+  return (
+    <nav className="main-navigation">
+      <div className="menu-main-container">
+        <ul id="primary-menu" className="menu">
+          <li className="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-703">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-713">
+            <Link to="/recipes">Recipes</Link>
+          </li>
+          <li className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-730">
+            <Link to="/login">Login</Link>
+          </li>
+          <li className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-731">
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBarMenu;
