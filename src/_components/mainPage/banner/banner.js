1c666928a2f8028a016f1276b4184c401744284a
sazgit <s.azfard@gmail.com>
2019-09-16 14:01:01 +0430

    banner of mainpage updated

---
+++ src/_components/mainPage/banner/banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../../App.css';
+import { Col, Row, Container} from 'react-bootstrap';
+import MainPageSearch from './mainPageSearch/searchMainPage';
+
+export default function Banner() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col className="banner">
+          <MainPageSearch/>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
