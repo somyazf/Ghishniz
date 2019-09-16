9f4fc0a906785a91e24651acc7357c8c34c46132
sazgit <s.azfard@gmail.com>
2019-09-16 14:18:45 +0430

    fetch data of authentication

---
+++ src/_service/authentication.service.js
@@ -0,0 +1,46 @@
+import {TIMEOUT_DELAY,AUTHHOST} from '../_constants';
+import axios from 'axios';
+
+export const authenticationService = {
+    login,
+    register
+}
+
+
+async function login (login,password) {
+    let timeout = null;
+    try{
+        const CancelToken = axios.CancelToken;
+        const source = CancelToken.source();
+        timeout = setTimeout(()=>{source.cancel()},TIMEOUT_DELAY);
+        const response = await axios({
+            url: `${AUTHHOST}/users/login`,
+            method: "post",
+            headers: { "content-type": "application/json" },
+            cancelToken: source.token,
+            data: {
+                login,
+                password
+            }
+        });
+        
+        if (response.status === 200) {
+            if (timeout) clearTimeout(timeout);
+            return response.data;
+        } else {
+            if (timeout) clearTimeout(timeout);
+            return Promise.reject({isTimeout:false,error: response.data});
+        }
+    } catch (error) {
+        if (timeout) clearTimeout(timeout);
+        if (axios.isCancel(error)) {
+          return Promise.reject({isTimeout:true});
+        } else {
+          return Promise.reject({isTimeout:false,error});
+        }
+    }
+}
+
+function register () {
+    return null
+}
