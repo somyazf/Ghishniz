a656f93dd6940ced2bf344bd4db819ba1811dbe2
sazgit <s.azfard@gmail.com>
2019-09-17 17:53:09 +0430

    create getRecipeInfo action

---
+++ src/_actions/getRecipesInfoAction.js
@@ -0,0 +1,31 @@
+import { getDataConstants } from "../_constants";
+import { getRecipesInfoService } from "../_service";
+
+export const getRecipesInfoAction = {
+    getRecipesInfo
+}
+debugger
+function getRecipesInfo(id) {
+    return dispatch => {
+        dispatch(loading());
+        getRecipesInfoService.getRecipesInfo(id).then(
+            response => {
+                dispatch(success(response));
+            },
+            error =>{
+                dispatch(failed(error));
+            }
+        )
+    }
+
+    function loading() { 
+        return { type: getDataConstants.FETCH_RECIPES_LOADING }; 
+    }
+    function success(data) { 
+        return { type: getDataConstants.FETCH_RECIPES_SUCCESS, data }; 
+    }
+    function failed(error) { 
+        return { type: getDataConstants.FETCH_RECIPES_FAILED, error }; 
+    }
+}
+
