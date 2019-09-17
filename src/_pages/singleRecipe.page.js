9985c38937a3972b313e32a3fdb8138c1e553bf9
sazgit <s.azfard@gmail.com>
2019-09-17 18:05:41 +0430

    delete unnecessary imports and connect to redux

--- src/_pages/singleRecipe.page.js
+++ src/_pages/singleRecipe.page.js
@@ -10,11 +10,10 @@ import EyeIcon from "../_components/mainPage/icons/eyeIcon";
 import FavoriteFood from "../_components/mainPage/icons/favorite";
 import Divider from '@material-ui/core/Divider';
 import sug1 from '../assets/sug1.jpg';
-import { Link } from "react-router-dom";
-import axios from 'axios';
+import { Link,withRouter} from "react-router-dom";
+import {connect} from "react-redux";
+import { getRecipesInfoAction } from "../_actions";
 
-const SpoonacularApi = "94be430aadf644f6a8c8c95abbcce4c1";
-const baseUrl = "https://api.spoonacular.com";
 const imgUrl = "https://webknox.com/recipeImages/";
 const useStyles = makeStyles(theme => ({
   root: {
@@ -48,31 +47,21 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const Recipe = (props) =>  {
-  const title = props.location.state.recipe;
-  const [data, setData] = useState([]);
-  const [url, setUrl] = useState(
-    `${baseUrl}/recipes/search?apiKey=${SpoonacularApi}&query=${title}`,
-);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  useEffect(() => {
-      const fetchData = async () => {
-          setIsError(false);
-          setIsLoading(true);
-          try {
-          const api_call = await axios(url);
-          setData(api_call.data.results[0]);
-          } catch (error) {
-              setIsError(true);
-          }
-          setIsLoading(false);
-      };
-      fetchData();
-    }, [url]);
-
+const SingleRecipe = (props) =>  {
+  // const title = props.location.state.recipe;
+  const id = props.location.state.id;
+  const {dispatch, error, loading, items} = props;
+  // const [data, setData] = useState([]);
+//   const [url, setUrl] = useState(
+//     `${baseUrl}/recipes/${id}/information?apiKey=${SpoonacularApi}`,
+// );
+  debugger
+  const componentDidMount = (id) => {
+    dispatch(getRecipesInfoAction.fetchRecipes(id))
+  }
+  
   const classes = useStyles();
-  const recipe = data;
+
     return <>
       <Grid container spacing={2}>
         <Grid item xs>
@@ -82,7 +71,7 @@ const Recipe = (props) =>  {
           <Grid className={classes.root}>
             <Grid item lg={6} className={classes.infoSec}>
               <Typography variant="h4" component="h3" align="left" gutterBottom>
-                {recipe.title}
+                {items.title}
               </Typography>
               <Rate/>
               <Grid className={classes.flexDivs}>
@@ -148,7 +137,8 @@ const Recipe = (props) =>  {
                 <Typography variant="h3" gutterBottom id= "direction">
                   Directions
                 </Typography>
-                <Typography variant="subtitle" gutterBottom>
+                <Typography variant="subtitle1" gutterBottom>
+                 {items.creditsText}
                   With machine running, drop garlic through feed tube of food processor to mince. Stop, add fresh basil, and process until chopped. Add lemon juice, oil, salt, and pepper and process to make thin wet rub. Spread both sides of pork chops with basil mixture. Let stand 15 to 30 minutes.
                   Prepare a medium-hot fire in grill. Brush the grate clean and oil the grate. Grill chops, over direct heat, turning once, to medium rare doneness, 5 to 6 minutes per side, or until the internal temperature reaches 145 degrees Fahrenheit, followed by a 3-minute rest time.
                 </Typography>
@@ -157,7 +147,7 @@ const Recipe = (props) =>  {
                 <Typography variant="h3" gutterBottom>
                   Reviews(126)
                 </Typography>
-                <Typography variant="subtitle" gutterBottom>
+                <Typography variant="subtitle1" gutterBottom>
                 Tim T. 5 days ago
 Definitely top with toasted pine nuts and shaved/grated Parmesan! Makes all the difference!!!
                 </Typography>
@@ -168,4 +158,13 @@ Definitely top with toasted pine nuts and shaved/grated Parmesan! Makes all the 
     </>
 };
 
-export default Recipe;
+const mapStateToProps = state => {
+  const {loading} = state.getDataReducer;
+  const {items} = state.getDataReducer;
+  const {error} = state.getDataReducer;
+  
+  return { loading, items, error };
+};
+
+const ConnectedRecipesPage = withRouter(connect(mapStateToProps)(SingleRecipe));
+export {ConnectedRecipesPage as SingleRecipe};
