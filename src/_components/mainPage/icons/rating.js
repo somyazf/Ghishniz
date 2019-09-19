efacfb9184bfb46dc30297c9886f341f8005c00a
sazgit <s.azfard@gmail.com>
2019-09-19 20:00:15 +0430

    changed name attribute

--- src/_components/mainPage/icons/rating.js
+++ src/_components/mainPage/icons/rating.js
@@ -3,31 +3,21 @@ import Rating from '@material-ui/lab/Rating';
 import Box from '@material-ui/core/Box';
 
 
- const SimpleRating = (props) => {
+const SimpleRating = (props) => {
   const [value, setValue] = React.useState(2);
-  
-
   return (
     <div>
-      {/* <RatingComponent
-        name="rate1"
-        starCount={5}
-        value={rating}
-        onStarClick={onStarClick}
-      /> */}
       <Box component="fieldset" mb={3} borderColor="transparent">
         <Rating
-          name={props.index}
+          name="simple-controlled"
           value={value}
           onChange={(event, newValue) => {
             setValue(newValue);
           }}
         />
       </Box>
-    
     </div>
   );
 }
-
 
 export default SimpleRating;
