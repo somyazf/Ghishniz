5f914c5f29f950201791c7001a3e3dea593f2815
sazgit <s.azfard@gmail.com>
2019-09-19 20:10:24 +0430

    changed logo image

--- src/_components/header/logo.js
+++ src/_components/header/logo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../../assets/ghishniz-logo.png";
+import Logo from "../../assets/newlogo-gishniz.png";
 import { Link } from "react-router-dom";
 import "../../App.css";
 
