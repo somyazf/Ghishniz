0cad7049710df4af155c26b45b4608d797650077
sazgit <s.azfard@gmail.com>
2019-09-21 13:33:08 +0430

    export all pages

--- src/_pages/index.js
+++ src/_pages/index.js
@@ -1,5 +1,12 @@
-export * from './home.page';
+import Home from "./home.page";
+export {Home};
 export * from './login.page';
 export * from './recipes.page';
 export * from './register.page';
+export * from './blogPosts.page';
+export * from './addNewPost.page';
 export * from './singleRecipe.page';
+export * from './blogOverview.page';
+export * from './userProfileLite.page';
+
+
