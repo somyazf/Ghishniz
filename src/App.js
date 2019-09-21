b936b2c5abb1b0743d8cd7d6fbe8fa14f8811891
sazgit <s.azfard@gmail.com>
2019-09-21 13:31:34 +0430

    map Rotues

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from './_pages/home.page';
-import {Recipes} from './_pages/recipes.page';
-import {SingleRecipe} from './_pages/singleRecipe.page';
-import {LoginPage} from "./_pages/login.page";
-import {RegisterPage} from "./_pages/register.page";
-import {history} from './_helpers/history';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Routes } from "./_routes";
+import { history } from './_helpers/history';
 import { connect } from "react-redux";
 import './App.css';
-import AddNewPost from './views/AddNewPost';
 
 const App = ({user}) =>{
-    return<>
-        <Router history={history}>
-          <Switch>
-            <Route path="/" exact component={Home}/>
-            <Route path="/recipes" component={Recipes}/>
-            <Route path="/recipe/:id" component={SingleRecipe} />
-            <Route path="/login" exact component={LoginPage}/>
-            <Route path="/register" exact component={RegisterPage}/>
-            <Route path="/addnewpost" exact component={AddNewPost}/>
-            
-          </Switch>
-          
-        </Router>
-      </>
+  return <>
+    <Router history={history}>
+      <Switch>
+        {Routes.map((route, index) => {
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />
+          );
+        })}
+      </Switch>
+    </Router>
+  </>
 }
 const mapStateToProps = state => {
   const {user} = state.authentication;
@@ -33,4 +30,4 @@ const mapStateToProps = state => {
 
 const ConnectedApp = connect(mapStateToProps)(App);
 
-export default  ConnectedApp;
+export default ConnectedApp;
