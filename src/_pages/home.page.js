ee4d66740bedd53b0b4bc7aca1b88f97393db8c2
sazgit <s.azfard@gmail.com>
2019-09-27 00:40:42 +0330

    update imports

--- src/_pages/home.page.js
+++ src/_pages/home.page.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Header} from '../components/header/header';
-import {Footer} from '../components/footer/footer';
-import Sidebar from '../components/mainPage/sidebar/sidebar';
-import MostFavoritedSec from '../components/mainPage/mostFavorited/mostFavorited';
-import RecentlyAddedSec from '../components/mainPage/recentlyAdded/recentlyAdded';
-import Banner from '../components/mainPage/banner/banner';
-import Offer  from "../components/mainPage/offerSection/offerSec";
-import ButtonBases  from "../components/mainPage/categoryGrid/categoryGrid";
+import {Header} from 'components/header/header';
+import {Footer} from 'components/footer/footer';
+import Sidebar from 'components/mainPage/sidebar/sidebar';
+import MostFavoritedSec from 'components/mainPage/mostFavorited/mostFavorited';
+import RecentlyAddedSec from 'components/mainPage/recentlyAdded/recentlyAdded';
+import Banner from 'components/mainPage/banner/banner';
+import Offer  from "components/mainPage/offerSection/offerSec";
+import ButtonBases  from "components/mainPage/categoryGrid/categoryGrid";
 import { Col, Row, Container, TabContainer} from 'react-bootstrap';
 
 export default function Home() {
