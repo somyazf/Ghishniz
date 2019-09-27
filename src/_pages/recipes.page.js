3507806b4437ba5895adc59b0bc0461b4313f706
somayehkhanbabaei <somayeh.khanbabaei21@gmail.com>
2019-09-27 21:33:38 +0330

    styled dashboard

--- src/_pages/recipes.page.js
+++ src/_pages/recipes.page.js
@@ -1,198 +1,223 @@
-import React, { useState } from 'react';
-import { Link,withRouter } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, withRouter } from "react-router-dom";
 import InnerSidebar from "../components/innerPageSidebar/innerPageSidebar";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
 import Rate from "components/mainPage/icons/rating";
 import Button from "@material-ui/core/Button";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import { getDataAction } from "_actions";
-import { Footer } from 'components/footer/footer';
-import Loading from 'assets/images/site/Noodles.gif';
+import { Footer } from "components/footer/footer";
+import Loading from "assets/images/site/Noodles.gif";
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import App from '../App';
 
 const imgUrl = "https://spoonacular.com/recipeImages/";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      padding: '2px 20px',
-      display: 'flex',
-      alignItems: 'center',
-      width: 550,
-      borderRadius: 25,
-      justifyContent: 'space-between',
-      marginTop: 50
-    },
-    input: {
-      marginLeft: theme.spacing(1),
-      flex: 1,
-      border: 'none',
-      padding: 5,
-      outline: 'none',
-      width: "100%",
-      fontSize: '1.2rem'
-    },
-    iconButton: {
-      padding: 10,
-    },
-    divider: {
-      height: 28,
-      margin: 4,
-    },
-    formWidth: {
-      width: '100%',
-    },
-    card: {
-      maxWidth: 280,
-      marginBottom: 20
-    },
-    cardTitle: {
-        fontSize: '1.2rem',
-    },
-    cardContent: {
-        background: '#ffbb00',
-        display: 'flex',
-        justifyContent: 'space-around',
-    },
-    cardAction: {
-        alignItems: 'baseline',
-        justifyContent: 'space-around',
-    },
-    media: {
-      height: 0,
-      paddingTop: "56.25%"
-    },
-    recipeSec: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
-    },
-    centeredDiv: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexWrap: 'wrap',
-    },
-    margin: {
-        margin: '40px 44px',
-    },
-    loadingImg:{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 'auto'
-    },
-    button: {
-        margin: theme.spacing(1),
-        color: '#fff !important'
-      },
-  }));
+  root: {
+    padding: "2px 20px",
+    display: "flex",
+    alignItems: "center",
+    width: 550,
+    borderRadius: 25,
+    justifyContent: "space-between",
+    marginTop: 50
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+    border: "none",
+    padding: 5,
+    outline: "none",
+    width: "100%",
+    fontSize: "1.2rem"
+  },
+  iconButton: {
+    padding: 10
+  },
+  divider: {
+    height: 28,
+    margin: 4
+  },
+  formWidth: {
+    width: "100%"
+  },
+  card: {
+    maxWidth: 280,
+    marginBottom: 20
+  },
+  cardTitle: {
+    fontSize: "1.2rem"
+  },
+  cardContent: {
+    background: "#ffbb00",
+    display: "flex",
+    justifyContent: "space-around"
+  },
+  cardAction: {
+    alignItems: "baseline",
+    justifyContent: "space-around"
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%"
+  },
+  recipeSec: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-between"
+  },
+  centeredDiv: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap"
+  },
+  margin: {
+    margin: "40px 44px"
+  },
+  loadingImg: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "auto"
+  },
+  button: {
+    margin: theme.spacing(1),
+    color: "#fff !important"
+  }
+}));
 
-const Recipes = (props) => {
-    const {dispatch, error, loading, items} = props;
-    const classes = useStyles();
-    const [query, setQuery] = useState('beef');
+const Recipes = props => {
+  const { dispatch, error, loading, items } = props;
+  const classes = useStyles();
+  const [query, setQuery] = useState("beef");
 
-    const submitHandler = async (event)=>{
-        event.preventDefault();
-        dispatch(getDataAction.fetchRecipes(query));
-    }
-    const handleChange = (event) => {
-        setQuery(event.target.value);
-    };
+  const submitHandler = async event => {
+    event.preventDefault();
+    dispatch(getDataAction.fetchRecipes(query));
+  };
+  const handleChange = event => {
+    setQuery(event.target.value);
+  };
 
-    return <>
-        <Grid container>
-            <Grid item xs={2}>
-                <InnerSidebar/>
-            </Grid>
-            <Grid item xs={10} >
-                <Grid className={classes.centeredDiv}>
-                    <Paper className={classes.root}>
-                        <form onSubmit={submitHandler}
-                        className={classes.formWidth}
-                        >
-                            <input
-                            type="text"
-                            value={query}
-                            onChange={handleChange}
-                            className={classes.input}
-                            />
-                        </form>
-                        <IconButton className={classes.iconButton} aria-label="search" type="submit">
-                            <SearchIcon />
-                        </IconButton>
-                    </Paper>
-                </Grid>
-                <Grid className={classes.margin}>
-                    <Grid className={classes.margin}>
-                        <Typography>
-                            {items.totalResults} Suggested Recipes
-                        </Typography>
-                    </Grid>
-                    <Grid>
-                        {error && <div>Something went wrong ...</div>}
-                        {loading ? (
-                            <div>
-                                <img src={Loading} alt="Loading" className={classes.loadingImg}/>
-                            </div>
-                        ) : (
-                                <Grid className={classes.centeredDiv}>
-                                    {items.results&&items.results.map(recipe => (
-                                        <Grid key={recipe.id} item lg={3}>
-                                            <Card className={classes.card}>
-                                                <CardMedia
-                                                    className={classes.media}
-                                                    image={`${imgUrl}${recipe.image}`}
-                                                    title={recipe.title}
-                                                />
-                                                <CardContent className={classes.cardContent}>
-                                                    <span>{recipe.readyInMinutes} Min</span>
-                                                    <span>{recipe.readyInMinutes} People</span>
-                                                    <span>{recipe.servings} Yield</span>
-                                                </CardContent>
-                                                <CardHeader
-                                                    title={recipe.title.length < 18 ? `${recipe.title}` : `${recipe.title.substring(0, 18)}...` }
-                                                />
-                                                <CardActions disableSpacing className={classes.cardAction}>
-                                                    <Rate/> 
-                                                    <Button variant="contained" color="secondary" className={classes.button} component={Link} to={{ 
-                                                        pathname: `/recipe/${recipe.id}`,
-                                                        state: { 
-                                                            recipe: recipe.title, 
-                                                            id: recipe.id
-                                                        }}} 
-                                                    >
-                                                        Detail
-                                                    </Button>
-                                                </CardActions>
-                                            </Card>
-                                        </Grid>
-                                    ))} 
-                                </Grid>
-                            )}
-                    </Grid>
-                </Grid>
-            </Grid>
+
+
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={2}>
+          <InnerSidebar />
         </Grid>
-        <Footer/>
+        <Grid item xs={10}>
+          <Grid className={classes.centeredDiv}>
+            <Paper className={classes.root}>
+              <form onSubmit={submitHandler} className={classes.formWidth}>
+                <input
+                  type="text"
+                  value={query}
+                  onChange={handleChange}
+                  className={classes.input}
+                />
+              </form>
+              <IconButton
+                className={classes.iconButton}
+                aria-label="search"
+                type="submit"
+              >
+                <SearchIcon />
+              </IconButton>
+            </Paper>
+          </Grid>
+          <Grid className={classes.margin}>
+            <Grid className={classes.margin}>
+              <Typography>{items.totalResults} Suggested Recipes</Typography>
+            </Grid>
+            <Grid>
+              {error && <div>Something went wrong ...</div>}
+              {loading ? (
+                <div>
+                  <img
+                    src={Loading}
+                    alt="Loading"
+                    className={classes.loadingImg}
+                  />
+                </div>
+              ) : (
+                <Grid className={classes.centeredDiv}>
+                  {items.results &&
+                    items.results.map(recipe => (
+                      <Grid key={recipe.id} item lg={3}>
+                        <Card className={classes.card}>
+                          <CardMedia
+                            className={classes.media}
+                            image={`${imgUrl}${recipe.image}`}
+                            title={recipe.title}
+                          />
+                          <CardContent className={classes.cardContent}>
+                            <span>{recipe.readyInMinutes} Min</span>
+                            <span>{recipe.readyInMinutes} People</span>
+                            <span>{recipe.servings} Yield</span>
+                          </CardContent>
+                          <CardHeader
+                            title={
+                              recipe.title.length < 18
+                                ? `${recipe.title}`
+                                : `${recipe.title.substring(0, 18)}...`
+                            }
+                          />
+                          <CardActions
+                            disableSpacing
+                            className={classes.cardAction}
+                          >
+                            <Rate />
+                            <Button
+                              variant="contained"
+                              color="secondary"
+                              className={classes.button}
+                              component={Link}
+                              to={{
+                                pathname: `/recipe/${recipe.id}`,
+                                state: {
+                                  recipe: recipe.title,
+                                  id: recipe.id
+                                }
+                              }}
+                            >
+                              Detail
+                            </Button>
+                          </CardActions>
+                        </Card>
+                      </Grid>
+                    ))}
+                </Grid>
+              )}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Footer />
     </>
-}
+  );
+};
 const mapStateToProps = state => {
-    const {loading} = state.getDataReducer;
-    const {items} = state.getDataReducer;
-    const {error} = state.getDataReducer;
-    
-    return { loading, items, error };
+  const { loading } = state.getDataReducer;
+  const { items } = state.getDataReducer;
+  const { error } = state.getDataReducer;
+
+  return { loading, items, error };
 };
 
 const ConnectedRecipesPage = withRouter(connect(mapStateToProps)(Recipes));
-export {ConnectedRecipesPage as Recipes};
+export { ConnectedRecipesPage as Recipes };
--- src/_pages/userDashboard/dashboard.js
+++ src/_pages/userDashboard/dashboard.js
@@ -2,8 +2,8 @@ import React from "react";
 import MainNavbar from "../../components/layout/MainNavbar/MainNavbar";
 import MainSidebar from "../../components/layout/MainSidebar/MainSidebar";
 import MainFooter from "../../components/layout/MainFooter";
-import { Row, Col } from "react-bootstrap";
-import { Container } from "@material-ui/core";
+import { Row, Col } from "shards-react";
+import { Container } from "shards-react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../shards-dashboard/styles/shards-dashboards.1.1.0.min.css";
 
@@ -11,20 +11,26 @@ const UserDashboard = (props) => {
     const Component = props.component;
   return (
     <>
-      <Container fluid>
-        <Row>
-          <Col lg={3}>
+      <Container fluid >
+        <Row className='mb-5'>
+          <Col lg={2}>
           <MainSidebar />
           </Col>
           
-          <Col lg={9} xl={12}>
+          <Col lg={10}>
             <MainNavbar />
+            <Component/>
 
-            {/* <MainFooter /> */}
           </Col>
+          
         </Row>
+        {/* <Row >
+        <Col lg={12}>
+          <MainFooter/> 
+
+          </Col>
+        </Row> */}
       </Container>
-      <Component/>
     </>
   );
 };
