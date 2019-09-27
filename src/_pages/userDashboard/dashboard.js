3a3f262bf67fbe17091e2b63d645ad54da311b30
somayehkhanbabaei <somayeh.khanbabaei21@gmail.com>
2019-09-27 15:21:18 +0330

    update

--- src/_pages/userDashboard/dashboard.js
+++ src/_pages/userDashboard/dashboard.js
@@ -13,17 +13,14 @@ const UserDashboard = (props) => {
     <>
       <Container fluid>
         <Row>
+          <Col lg={3}>
           <MainSidebar />
-          <Col
-            className="main-content p-0"
-            lg={{ size: 10, offset: 2 }}
-            md={{ size: 9, offset: 3 }}
-            sm="12"
-            tag="main"
-          >
+          </Col>
+          
+          <Col lg={9} xl={12}>
             <MainNavbar />
 
-            <MainFooter />
+            {/* <MainFooter /> */}
           </Col>
         </Row>
       </Container>
