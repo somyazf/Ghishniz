773420f919d82393b235fcb79f7905c5ca743d5b
somayehkhanbabaei <somayeh.khanbabaei21@gmail.com>
2019-09-28 17:29:43 +0330

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Routes } from "./_routes";
-import withTracker from "./withTracker";
 import { history } from './_helpers/history';
 import { connect } from "react-redux";
 import './App.css';
-import { Home, Recipes, SingleRecipe, AddNewPost, BlogOverview, UserProfileLite, BlogPosts } from './_pages';
-import LoginPages from './_pages/login_page';
-import RegisterPages from './_pages/register_page';
+import { BlogOverview} from './_pages';
 import UserDashboard from './_pages/userDashboard/dashboard';
-import { DefaultLayout } from './layouts';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./shards-dashboard/styles/shards-dashboards.1.1.0.min.css";
 
@@ -32,16 +28,7 @@ const App = ({user}) =>{
               {...rest}
               component = {Layout ? withLayout(props=><Layout {...props}><Component {...props}/></Layout>): withLayout(props=><Component {...props}/>)}
             />)}
-            {/* <Route path="/" exact component={Home}/>
-            <Route path="/recipes" component={Recipes}/>
-            <Route path="/recipe/:id" component={SingleRecipe} />
-            <Route path="/login" component={LoginPages}/>
-            <Route path="/register" component={RegisterPages}/>
-            <Route path="/add-new-post" component={AddNewPost}/>  
-            <Route path="/blog-posts" component={BlogPosts}/>
-            <Route path="/blog-overview" component={BlogOverview}/>
-            <Route path="/user-profile-lite" component={UserProfileLite}/> */}
-            <Route path="/dashboard" render={ ()=> <UserDashboard  component={BlogOverview} />}/>
+            
           </Switch>
 
         </Router>
--- src/components/mainPage/mainPageSearch/searchMainPage.js
+++ src/components/mainPage/mainPageSearch/searchMainPage.js
@@ -1,58 +1,3 @@
-// import React from "react";
-// import SearchIcon from "@material-ui/icons/Search";
-// import Input from "@material-ui/core/Input";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import InputAdornment from "@material-ui/core/InputAdornment";
-// import FormControl from "@material-ui/core/FormControl";
-// import { makeStyles } from "@material-ui/core/styles";
-// import Paper from "@material-ui/core/Paper";
-
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     padding: '2px 20px',
-//       display: 'flex',
-//       alignItems: 'center',
-//       width: 550,
-//       borderRadius: 50,
-//       justifyCntent: 'space-between',
-
-//   },
-//   formControl: {
-//     margin: theme.spacing(1),
-//     width:"100%",
-//   },
-//   label: {
-//     fontSize:'30px',
-//   },
-//   searchIcon: {
-//       fontSize:"20px",
-//       marginTop:"40px",
-//   }
-// }));
-
-// function MainPageSearch() {
-//   const classes = useStyles();
-//   return (
-//     <Paper className={classes.root}>
-//       <FormControl className={classes.formControl}>
-//         <InputLabel className={classes.label} htmlFor="input-with-icon-adornment">Search</InputLabel>
-//         <Input 
-//         id="input-with-icon-adornment"
-//         startAdornment={
-//           <InputAdornment>
-//             <SearchIcon/>
-//           </InputAdornment>
-//         }
-//         className={classes.searchIcon}
-//         />
-//       </FormControl>
-//     </Paper>
-  
-//   );
-// }
-
-// export default MainPageSearch;
-
 import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
@@ -63,8 +8,6 @@ import FormControl from "@material-ui/core/FormControl";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import {connect} from "react-redux";
-import { getDataAction } from "_actions";
-import IconButton from '@material-ui/core/IconButton';
 
 
 const useStyles = makeStyles(theme => ({
@@ -105,19 +48,6 @@ function MainPageSearch(props) {
 
   return (
     <Paper className={classes.root}>
-        {/* <form onSubmit={submitHandler}
-        className={classes.formWidth}
-        >
-            <input
-            type="text"
-            value={query}
-            onChange={handleChange}
-            className={classes.input}
-            />
-        </form>
-        <IconButton className={classes.iconButton} aria-label="search" type="submit">
-            <SearchIcon />
-        </IconButton> */}
       <FormControl className={classes.formControl}>
         <InputLabel
           className={classes.label}
