a403a5bb81ae90273b15d47c6f7a6b8ebfe2fa20
sazgit <s.azfard@gmail.com>
2019-09-28 20:24:57 +0330

    add key to map

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Routes } from "./_routes";
-import { history } from './_helpers/history';
+import { Router, Switch, Route } from 'react-router-dom';
+import { Routes } from "_routes";
+import { history } from '_helpers/history';
 import { connect } from "react-redux";
-import './App.css';
-import { BlogOverview} from './_pages';
-import UserDashboard from './_pages/userDashboard/dashboard';
+import 'App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./shards-dashboard/styles/shards-dashboards.1.1.0.min.css";
+import "shards-dashboard/styles/shards-dashboards.1.1.0.min.css";
 
 
 const withLayout = (WrappedComponent, options = {}) => {
-
-  // eslint-disable-next-line
   const HOC = (props) => <WrappedComponent {...props} />;
-
-
   return HOC;
 };
 
@@ -23,14 +17,15 @@ const App = ({user}) =>{
     return<>
         <Router history={history}>
           <Switch>
-            {Routes.map(({component:Component,layout:Layout, ...rest})=><Route 
-              
+            {Routes.map(({component:Component,layout:Layout, ...rest},index)=><Route 
+              key={index}
               {...rest}
-              component = {Layout ? withLayout(props=><Layout {...props}><Component {...props}/></Layout>): withLayout(props=><Component {...props}/>)}
+              component = {Layout ? withLayout(props=>
+                <Layout {...props}><Component {...props}/></Layout>): 
+                withLayout(props=><Component {...props}/>
+              )}
             />)}
-            
           </Switch>
-
         </Router>
       </>
 }
