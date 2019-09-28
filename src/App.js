5158dd5f1ab7cfb00fb1750f362132100788e09d
somayehkhanbabaei <somayeh.khanbabaei21@gmail.com>
2019-09-28 12:08:58 +0330

    map on routes

--- src/App.js
+++ src/App.js
@@ -13,19 +13,33 @@ import { DefaultLayout } from './layouts';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./shards-dashboard/styles/shards-dashboards.1.1.0.min.css";
 
+
+const withLayout = (WrappedComponent, options = {}) => {
+
+  // eslint-disable-next-line
+  const HOC = (props) => <WrappedComponent {...props} />;
+
+
+  return HOC;
+};
+
 const App = ({user}) =>{
     return<>
         <Router history={history}>
           <Switch>
-            <Route path="/" exact component={Home}/>
+            {Routes.map(({component:Component,layout:Layout, ...rest})=><Route
+              {...rest}
+              component = {Layout ? withLayout(props=><Layout {...props}><Component {...props}/></Layout>): withLayout(props=><Component {...props}/>)}
+            />)}
+            {/* <Route path="/" exact component={Home}/>
             <Route path="/recipes" component={Recipes}/>
             <Route path="/recipe/:id" component={SingleRecipe} />
             <Route path="/login" component={LoginPages}/>
             <Route path="/register" component={RegisterPages}/>
-            <Route path="/add-new-post" component={AddNewPost}/>
+            <Route path="/add-new-post" component={AddNewPost}/>  
             <Route path="/blog-posts" component={BlogPosts}/>
             <Route path="/blog-overview" component={BlogOverview}/>
-            <Route path="/user-profile-lite" component={UserProfileLite}/>
+            <Route path="/user-profile-lite" component={UserProfileLite}/> */}
             <Route path="/dashboard" render={ ()=> <UserDashboard  component={BlogOverview} />}/>
           </Switch>
 
