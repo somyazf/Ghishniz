f9e79c25dcb1e44ad8479604dab29a80de7259a4
somayehkhanbabaei <somayeh.khanbabaei21@gmail.com>
2019-09-28 12:08:41 +0330

    updated

--- src/withTracker.js
+++ src/withTracker.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-
-GoogleAnalytics.initialize(process.env.REACT_APP_GAID || "UA-115105611-2");
 const withTracker = (WrappedComponent, options = {}) => {
 
   // eslint-disable-next-line
   const HOC = (props) => <WrappedComponent {...props} />;
-
-
   return HOC;
 };
 
