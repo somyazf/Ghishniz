63f65a7b3d79234624de98aa2cca03ed56f0433b
sazgit <s.azfard@gmail.com>
2019-09-29 12:38:14 +0330

    update AUTHHOST

--- src/_constants/other.constants.js
+++ src/_constants/other.constants.js
@@ -1,7 +1,7 @@
 export const apiKey = `94be430aadf644f6a8c8c95abbcce4c1`;
 export const HOST = `https://api.spoonacular.com/recipes/search`;
 export const RecipeInformationHost = `https://api.spoonacular.com/recipes/`;
-export const AUTHHOST = 'https://api.backendless.com/F0ED3E53-B85E-7A1C-FFCE-5AAF8615B100/2ECA2E59-47D0-2D81-FFA0-1B582B7F9B00';
+export const AUTHHOST = 'https://api.backendless.com/F0ED3E53-B85E-7A1C-FFCE-5AAF8615B100/EE6FA35F-F79F-4B42-B8D8-C62F4CFD41A3';
 export const TIMEOUT_DELAY = 10000;
 export const realtimeDate = {
     userData: null
