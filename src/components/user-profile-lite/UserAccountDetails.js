b42cbad26351f4fb9fa4697ff9da72d9eca21e8e
somayehkhanbabaei <somayeh.khanbabaei21@gmail.com>
2019-09-30 13:18:42 +0330

    access to input

--- src/components/user-profile-lite/UserAccountDetails.js
+++ src/components/user-profile-lite/UserAccountDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component, useState } from "react";
 import PropTypes from "prop-types";
 import {
   Card,
@@ -15,8 +15,22 @@ import {
   Button
 } from "shards-react";
 
-const UserAccountDetails = ({ title }) => (
-  <Card small className="mb-4">
+
+
+const UserAccountDetails = ({ title }) => {
+
+  const [values,setValues] = useState({
+    name: '',
+    lastName: '',
+    email: '',
+    password: ''
+})
+
+const handleChange = ({target:{name,value}}) => {
+  setValues({ ...values, [name]: value });
+};
+  return <>
+<Card small className="mb-4">
     <CardHeader className="border-bottom">
       <h6 className="m-0">{title}</h6>
     </CardHeader>
@@ -30,20 +44,11 @@ const UserAccountDetails = ({ title }) => (
                 <Col md="6" className="form-group">
                   <label htmlFor="feFirstName">First Name</label>
                   <FormInput
+                    name="name"
                     id="feFirstName"
                     placeholder="First Name"
-                    value="Sierra"
-                    onChange={() => {}}
-                  />
-                </Col>
-                {/* Last Name */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feLastName">Last Name</label>
-                  <FormInput
-                    id="feLastName"
-                    placeholder="Last Name"
-                    value="Brooks"
-                    onChange={() => {}}
+                    value={values.name}
+                    onChange={handleChange}
                   />
                 </Col>
               </Row>
@@ -52,11 +57,12 @@ const UserAccountDetails = ({ title }) => (
                 <Col md="6" className="form-group">
                   <label htmlFor="feEmail">Email</label>
                   <FormInput
+                    name="email"
                     type="email"
                     id="feEmail"
                     placeholder="Email Address"
-                    value="sierra@example.com"
-                    onChange={() => {}}
+                    value={values.email}
+                    onChange={handleChange}
                     autoComplete="email"
                   />
                 </Col>
@@ -64,59 +70,19 @@ const UserAccountDetails = ({ title }) => (
                 <Col md="6" className="form-group">
                   <label htmlFor="fePassword">Password</label>
                   <FormInput
+                    name="password"
                     type="password"
                     id="fePassword"
                     placeholder="Password"
-                    value="EX@MPL#P@$$w0RD"
-                    onChange={() => {}}
+                    value={values.password}
+                    onChange={handleChange}
                     autoComplete="current-password"
                   />
                 </Col>
               </Row>
-              <FormGroup>
-                <label htmlFor="feAddress">Address</label>
-                <FormInput
-                  id="feAddress"
-                  placeholder="Address"
-                  value="1234 Main St."
-                  onChange={() => {}}
-                />
-              </FormGroup>
-              <Row form>
-                {/* City */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feCity">City</label>
-                  <FormInput
-                    id="feCity"
-                    placeholder="City"
-                    onChange={() => {}}
-                  />
-                </Col>
-                {/* State */}
-                <Col md="4" className="form-group">
-                  <label htmlFor="feInputState">State</label>
-                  <FormSelect id="feInputState">
-                    <option>Choose...</option>
-                    <option>...</option>
-                  </FormSelect>
-                </Col>
-                {/* Zip Code */}
-                <Col md="2" className="form-group">
-                  <label htmlFor="feZipCode">Zip</label>
-                  <FormInput
-                    id="feZipCode"
-                    placeholder="Zip"
-                    onChange={() => {}}
-                  />
-                </Col>
-              </Row>
-              <Row form>
-                {/* Description */}
-                <Col md="12" className="form-group">
-                  <label htmlFor="feDescription">Description</label>
-                  <FormTextarea id="feDescription" rows="5" />
-                </Col>
-              </Row>
+            
+             
+             
               <Button theme="accent">Update Account</Button>
             </Form>
           </Col>
@@ -124,7 +90,11 @@ const UserAccountDetails = ({ title }) => (
       </ListGroupItem>
     </ListGroup>
   </Card>
-);
+  </>
+}
+
+  
+
 
 UserAccountDetails.propTypes = {
   /**
