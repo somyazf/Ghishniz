d479b0c6b805baf05df350a96db6e390d5ff48b2
sazgit <s.azfard@gmail.com>
2019-10-01 23:33:46 +0330

    set publish onclick

--- src/components/add-new-post/SidebarActions.js
+++ src/components/add-new-post/SidebarActions.js
@@ -1,5 +1,3 @@
-/* eslint jsx-a11y/anchor-is-valid: 0 */
-
 import React from "react";
 import PropTypes from "prop-types";
 import {
@@ -10,56 +8,60 @@ import {
   ListGroupItem,
   Button
 } from "shards-react";
+import { connect } from 'react-redux';
 
-const SidebarActions = ({ title }) => (
-  <Card small className="mb-3">
-    <CardHeader className="border-bottom">
-      <h6 className="m-0">{title}</h6>
-    </CardHeader>
+const SidebarActions = ({ title, addPost}) => {
 
-    <CardBody className="p-0">
-      <ListGroup flush>
-        <ListGroupItem className="p-3">
-          <span className="d-flex mb-2">
-            <i className="material-icons mr-1">flag</i>
-            <strong className="mr-1">Status:</strong> Draft{" "}
-            <a className="ml-auto" href="#">
-              Edit
-            </a>
-          </span>
-          <span className="d-flex mb-2">
-            <i className="material-icons mr-1">visibility</i>
-            <strong className="mr-1">Visibility:</strong>{" "}
-            <strong className="text-success">Public</strong>{" "}
-            <a className="ml-auto" href="#">
-              Edit
-            </a>
-          </span>
-          <span className="d-flex mb-2">
-            <i className="material-icons mr-1">calendar_today</i>
-            <strong className="mr-1">Schedule:</strong> Now{" "}
-            <a className="ml-auto" href="#">
-              Edit
-            </a>
-          </span>
-          <span className="d-flex">
-            <i className="material-icons mr-1">score</i>
-            <strong className="mr-1">Readability:</strong>{" "}
-            <strong className="text-warning">Ok</strong>
-          </span>
-        </ListGroupItem>
-        <ListGroupItem className="d-flex px-3 border-0">
-          <Button outline theme="accent" size="sm">
-            <i className="material-icons">save</i> Save Draft
-          </Button>
-          <Button theme="accent" size="sm" className="ml-auto">
-            <i className="material-icons">file_copy</i> Publish
-          </Button>
-        </ListGroupItem>
-      </ListGroup>
-    </CardBody>
-  </Card>
-);
+  return <>
+    <Card small className="mb-3">
+      <CardHeader className="border-bottom">
+        <h6 className="m-0">{title}</h6>
+      </CardHeader>
+
+      <CardBody className="p-0">
+        <ListGroup flush>
+          <ListGroupItem className="p-3">
+            <span className="d-flex mb-2">
+              <i className="material-icons mr-1">flag</i>
+              <strong className="mr-1">Status:</strong> Draft{" "}
+              <a className="ml-auto" href="#">
+                Edit
+              </a>
+            </span>
+            <span className="d-flex mb-2">
+              <i className="material-icons mr-1">visibility</i>
+              <strong className="mr-1">Visibility:</strong>{" "}
+              <strong className="text-success">Public</strong>{" "}
+              <a className="ml-auto" href="#">
+                Edit
+              </a>
+            </span>
+            <span className="d-flex mb-2">
+              <i className="material-icons mr-1">calendar_today</i>
+              <strong className="mr-1">Schedule:</strong> Now{" "}
+              <a className="ml-auto" href="#">
+                Edit
+              </a>
+            </span>
+            <span className="d-flex">
+              <i className="material-icons mr-1">score</i>
+              <strong className="mr-1">Readability:</strong>{" "}
+              <strong className="text-warning">Ok</strong>
+            </span>
+          </ListGroupItem>
+          <ListGroupItem className="d-flex px-3 border-0">
+            <Button outline theme="accent" size="sm">
+              <i className="material-icons">save</i> Save Draft
+            </Button>
+            <Button theme="accent" size="sm" className="ml-auto" onClick={addPost}>
+              <i className="material-icons">file_copy</i> Publish
+            </Button>
+          </ListGroupItem>
+        </ListGroup>
+      </CardBody>
+    </Card>
+  </>
+};
 
 SidebarActions.propTypes = {
   /**
@@ -72,4 +74,5 @@ SidebarActions.defaultProps = {
   title: "Actions"
 };
 
-export default SidebarActions;
+export default connect()(SidebarActions)
+// export default SidebarActions;
