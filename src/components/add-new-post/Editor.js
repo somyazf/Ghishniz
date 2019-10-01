43883ff5e8e48348dcaadcb0f98f617b1d0a55da
sazgit <s.azfard@gmail.com>
2019-10-01 23:31:56 +0330

    form values control

--- src/components/add-new-post/Editor.js
+++ src/components/add-new-post/Editor.js
@@ -1,5 +1,4 @@
-import React from "react";
-import ReactQuill from "react-quill";
+import React, { useState } from "react";
 import { 
   Card, 
   CardBody, 
@@ -8,6 +7,7 @@ import {
   InputGroup,
   InputGroupText,
   InputGroupAddon, 
+  FormTextarea 
 } from "shards-react";
 import { Col, Row } from 'react-bootstrap';
 
@@ -15,46 +15,101 @@ import "react-quill/dist/quill.snow.css";
 import "assets/styles/quill.css";
 import MultipleInput from "./multipleInput";
 
-const Editor = () => (
-  <Card small className="mb-3">
-    <CardBody>
-      <Form className="add-new-post">
-        <FormInput size="lg" className="mb-3" placeholder="Your Post Title" />
-        <ReactQuill className="add-new-post__editor mb-1" />
-      </Form>
-      <Row>
-        <h4>Ingredients:</h4>
-        <Col>
-          <MultipleInput/>
-        </Col>
-      </Row>
-      <Row>
-        <h4>Additional Information:</h4>
-        <Col>
-          <InputGroup className="mb-2">
-            <InputGroupAddon type="prepend">
-              <InputGroupText>Preperation Time</InputGroupText>
-            </InputGroupAddon>
-            <FormInput />
-            <InputGroupAddon type="append">
-              <InputGroupText>Min</InputGroupText>
-            </InputGroupAddon>
-          </InputGroup>
-        </Col>
-        <Col>
-        <InputGroup className="mb-2">
-              <InputGroupAddon type="prepend">
-                <InputGroupText>Serving People</InputGroupText>
-              </InputGroupAddon>
-              <FormInput />
-              <InputGroupAddon type="append">
-                <InputGroupText></InputGroupText>
-              </InputGroupAddon>
-            </InputGroup>
-        </Col>
-      </Row>
-    </CardBody>
-  </Card>
-);
+const Editor = ({
+  valuesTitle,
+  valuesDirection,
+  valuesReadyInMinutes,
+  valuesServings,
+  imagePreviewUrl,
+  handleChange,
+  handleImageChange
+}) => {
+
+  const blankIngredinet = {ingredient:''};
+  const [inputs, setInputs] = useState([
+      {...blankIngredinet}
+  ]);
+
+  const addInput = () =>{
+      setInputs([...inputs, {...blankIngredinet}]);
+  }
+
+  const inputHandleChange = (event) => {
+      const updatedInputs = [...inputs];
+      updatedInputs[event.target.name] = event.target.value;
+      setInputs(updatedInputs); 
+  }
+
+  const handleRemove = (id) =>{
+      const items = [...inputs]
+      items.splice(id, 1);
+      setInputs(items);
+  }  
+
+  let imagePreview = null;
+  if (imagePreviewUrl) {
+    imagePreview = (<img src={imagePreviewUrl} />);
+  } else {
+    imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
+  }
+
+  return <>
+    <Card small className="mb-3">
+      <CardBody>
+        <Form className="add-new-post">
+          <FormInput size="lg" className="mb-3" placeholder="Post Title" name="title" onChange={handleChange}/>
+          <h5>Direction:</h5>
+          <FormTextarea size="lg" className="mb-3" name="direction" value={valuesDirection} onChange={handleChange}/>
+          <Row>
+            <Col>
+              <h5>Ingredients:</h5>
+              <MultipleInput
+                inputs={inputs}
+                addInput={addInput}
+                handleChange={inputHandleChange}
+                handleRemove={handleRemove}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h5>Additional Information:</h5>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <InputGroup className="mb-2">
+                <InputGroupAddon type="prepend">
+                  <InputGroupText>Preperation Time</InputGroupText>
+                </InputGroupAddon>
+                <FormInput name="readyInMinutes" value={valuesReadyInMinutes} onChange={handleChange}/>
+                <InputGroupAddon type="append">
+                  <InputGroupText>Min</InputGroupText>
+                </InputGroupAddon>
+              </InputGroup>
+            </Col>
+            <Col>
+            <InputGroup className="mb-2">
+                  <InputGroupAddon type="prepend">
+                    <InputGroupText>Serving People</InputGroupText>
+                  </InputGroupAddon>
+                  <FormInput name="servings" value={valuesServings} onChange={handleChange}/>
+                  <InputGroupAddon type="append">
+                    <InputGroupText></InputGroupText>
+                  </InputGroupAddon>
+                </InputGroup>
+            </Col>
+          </Row>
+          <Row>
+            <FormInput name="readyInMinutes" type="file" name="image" onChange={handleImageChange}/>
+            <div className="imgPreview">
+              {imagePreview}
+            </div>
+          </Row>
+        </Form>
+      </CardBody>
+    </Card>
+  </>
+}
 
 export default Editor;
